8e639de04449baef490c3a59b88ed20891bb420e
Vinicius Alberkovics <alberkovicsvinicius@gmail.com>
2023-11-29 18:23:46 -0300

    feat: update font to gov pattern

--- front-issuer/src/theme/create-typography.js
+++ front-issuer/src/theme/create-typography.js
@@ -1,6 +1,26 @@
-export const createTypography = () => {
+/**
+ * @param {import('@mui/material/styles/createTheme').Theme} theme
+ * @returns {import('@mui/material/styles/createTypography').TypographyOptions}
+ */
+export const createTypography = (theme) => {
+  const fontSizeBasePx = '14px';
+  const fontSizeScaleDown01 = multiplyFontSize(fontSizeBasePx, 'px', 0.833);
+  const fontSizeScaleDown02 = multiplyFontSize(fontSizeBasePx, 'px', 0.694);
+  const fontSizeScaleDown03 = multiplyFontSize(fontSizeBasePx, 'px', 0.579);
+  const fontSizeScaleUp01 = multiplyFontSize(fontSizeBasePx, 'px', 1.2);
+  const fontSizeScaleUp02 = multiplyFontSize(fontSizeBasePx, 'px', 1.44);
+  const fontSizeScaleUp03 = multiplyFontSize(fontSizeBasePx, 'px', 1.728);
+  const fontSizeScaleUp04 = multiplyFontSize(fontSizeBasePx, 'px', 2.074);
+  const fontSizeScaleUp05 = multiplyFontSize(fontSizeBasePx, 'px', 2.488);
+  const fontSizeScaleUp06 = multiplyFontSize(fontSizeBasePx, 'px', 2.986);
+  const fontSizeScaleUp07 = multiplyFontSize(fontSizeBasePx, 'px', 3.583);
+  const fontSizeScaleUp08 = multiplyFontSize(fontSizeBasePx, 'px', 4.3);
+  const fontSizeScaleUp09 = multiplyFontSize(fontSizeBasePx, 'px', 5.16);
+  const fontSizeScaleUp10 = multiplyFontSize(fontSizeBasePx, 'px', 6.192);
+  const fontSizeScaleUp11 = multiplyFontSize(fontSizeBasePx, 'px', 7.43);
   return {
-    fontFamily: '"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+    fontFamily: 'Rawline, "Raleway", sans-serif',
+    fontSize: fontSizeBasePx,
     body1: {
       fontSize: '1rem',
       fontWeight: 400,
@@ -37,40 +57,65 @@ export const createTypography = () => {
       textTransform: 'uppercase'
     },
     h1: {
-      fontFamily: '\'Plus Jakarta Sans\', sans-serif',
+      fontFamily: 'Rawline, "Raleway",  sans-serif',
       fontWeight: 700,
-      fontSize: '3.5rem',
-      lineHeight: 1.2
+      fontSize: fontSizeScaleUp04,
+      lineHeight: 1.2,
+      [theme.breakpoints.up('sm')]: {
+        fontSize: fontSizeScaleUp06,
+      },
     },
     h2: {
-      fontFamily: '\'Plus Jakarta Sans\', sans-serif',
+      fontFamily: 'Rawline, "Raleway",  sans-serif',
       fontWeight: 700,
-      fontSize: '3rem',
-      lineHeight: 1.2
+      fontSize: fontSizeScaleUp03,
+      lineHeight: 1.2,
+      [theme.breakpoints.up('sm')]: {
+        fontSize: fontSizeScaleUp05,
+      },
     },
     h3: {
-      fontFamily: '\'Plus Jakarta Sans\', sans-serif',
+      fontFamily: 'Rawline, "Raleway",  sans-serif',
       fontWeight: 700,
-      fontSize: '2.25rem',
-      lineHeight: 1.2
+      fontSize: fontSizeScaleUp03,
+      lineHeight: 1.2,
+      [theme.breakpoints.up('sm')]: {
+        fontSize: fontSizeScaleUp04,
+      },
     },
     h4: {
-      fontFamily: '\'Plus Jakarta Sans\', sans-serif',
+      fontFamily: 'Rawline, "Raleway",  sans-serif',
       fontWeight: 700,
-      fontSize: '2rem',
-      lineHeight: 1.2
+      fontSize: fontSizeScaleUp01,
+      lineHeight: 1.2,
+      [theme.breakpoints.up('sm')]: {
+        fontSize: fontSizeScaleUp03,
+      },
     },
     h5: {
-      fontFamily: '\'Plus Jakarta Sans\', sans-serif',
+      fontFamily: 'Rawline, "Raleway",  sans-serif',
       fontWeight: 700,
-      fontSize: '1.5rem',
-      lineHeight: 1.2
+      fontSize: fontSizeBasePx,
+      lineHeight: 1.2,
+      textTransform: 'uppercase',
+      [theme.breakpoints.up('sm')]: {
+        fontSize: fontSizeScaleUp02,
+      },
     },
     h6: {
-      fontFamily: '\'Plus Jakarta Sans\', sans-serif',
+      fontFamily: 'Rawline, "Raleway",  sans-serif',
       fontWeight: 700,
-      fontSize: '1.125rem',
-      lineHeight: 1.2
+      fontSize: fontSizeScaleDown01,
+      lineHeight: 1.2,
+      textTransform: 'uppercase',
+      [theme.breakpoints.up('sm')]: {
+        fontSize: fontSizeScaleUp01,
+      },
     }
   };
 };
+
+function multiplyFontSize(font, unit, multiplyBy) {
+  const fontSize = Number(font.replace(unit, ''));
+  return `${fontSize * multiplyBy}${unit}`;
+}
