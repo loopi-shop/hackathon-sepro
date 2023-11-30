6b18553008acb4cc71f9128f5b5a6ae181e3d0cf
Ítalo Sampaio <italolevy@hotmail.com>
2023-11-30 15:36:01 -0500

    chore: add Font Awesome

--- front-issuer/src/pages/_app.js
+++ front-issuer/src/pages/_app.js
@@ -31,13 +31,12 @@ const App = (props) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <link href="https://fonts.cdnfonts.com/css/rawline" rel="stylesheet" />
-        <title>
-          Título Público Federal
-        </title>
-        <meta
-          name="viewport"
-          content="initial-scale=1, width=device-width"
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
         />
+        <title>Título Público Federal</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <SnackbarProvider maxSnack={10}>
