013a176167bd8a4b60c15f0c7aeccbfb16bacaf3
Ítalo Sampaio <italolevy@hotmail.com>
2023-12-01 10:17:11 -0500

    feat: add header to auth layout

---
+++ front-issuer/src/layouts/auth/header.js
@@ -0,0 +1,16 @@
+export const AuthHeader = () => {
+  return (
+    <header className="br-header">
+      <div className="container-lg">
+        <div className="header-bottom">
+          <div className="header-menu">
+            <div className="header-info">
+              <div className="header-title">TPFchain</div>
+              <div className="header-subtitle">Tesouro direto pro mundo</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
