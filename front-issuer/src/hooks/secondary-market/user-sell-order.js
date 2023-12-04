730c972aa40ed5d00fa4e4fc3bd1c684b215d63c
Vinicius Alberkovics <alberkovicsvinicius@gmail.com>
2023-12-04 19:50:49 -0300

    feat: secondary market start createListing

--- front-issuer/src/hooks/secondary-market/user-sell-order.js
+++ front-issuer/src/hooks/secondary-market/user-sell-order.js
@@ -1,15 +1,46 @@
 import { useEffect, useMemo, useState } from 'react';
+import { useSecondaryMarketContext } from 'src/contexts/secondary-martket-context';
+import { useAuth } from '../use-auth';
+import { useSDK } from '@metamask/sdk-react';
+import { useTPF } from '../use-tpf';
 
 export function useSellOrder() {
   const [options, setOptions] = useState([]);
   const [values, setValues] = useState({ asset: '', quantity: 2, totalPrice: 2000 });
+  const { approve, createListing, waitTransaction } = useSecondaryMarketContext();
+  const { tpfs, balanceOf } = useTPF();
+  const { user } = useAuth();
+  const { sdk } = useSDK();
+
+  const getTPFs = async () => {
+    console.log('getTPFs', tpfs);
+    if (!tpfs.isLoading) {
+      const tempOpt = tpfs.data.map((tpf) => ({
+        value: tpf.symbol,
+        id: tpf.contractAddress,
+      }));
+      const opts = await Promise.all(tempOpt.map(async (tpf) => {
+        console.log('tpf: ', tpf)
+        const balance = await balanceOf({ accountAddress: user.publicKey, contractAddress: tpf.id })
+        if (balance > 0) {
+          return {
+            value: tpf.value,
+            id: tpf.id,
+          }
+        }
+      }));
+      console.log('options: ', opts.filter((opt) => opt !== undefined))
+      setOptions(opts.filter((opt) => opt !== undefined));
+    }
+  }
 
   useEffect(() => {
-    setOptions([
-      { value: 'Symbol01', id: 1 },
-      { value: 'Symbol02', id: 2 }
-    ]);
+    getTPFs();
   }, []);
+
+  useEffect(() => {
+    getTPFs();
+  }, [tpfs]);
 
   const totalAssets = useMemo(() => {
     if (!values.asset) {
@@ -39,8 +70,48 @@ export function useSellOrder() {
       const newValues = Object.assign({}, { ...values, [key]: val });
       setValues(newValues);
     },
-    onClickSell: () => {
-      //TODO
+    onClickSell: async () => {
+      console.log('onClickSell', {
+        amount: values.quantity,
+        from: user.publicKey,
+        asset: values.asset,
+        price: values.totalPrice,
+        from: user.publicKey,
+      })
+      try {
+        const txAllowance = await approve({
+          amount: values.quantity,
+          from: user.publicKey,
+          asset: values.asset,
+        })
+        console.debug('approve tx: ', txAllowance);
+        const txHash = await sdk.getProvider().request({
+          method: 'eth_sendTransaction',
+          params: [txAllowance]
+        });
+        await waitTransaction({ txHash });
+      } catch (error) {
+        console.error('approve error: ', error);
+        return;
+      }
+
+      try {
+        const tx = await createListing({
+          amount: values.quantity,
+          price: values.totalPrice,
+          token: values.asset,
+          from: user.publicKey,
+        });
+        console.debug('createListing tx: ', tx);
+        const txHash = await sdk.getProvider().request({
+          method: 'eth_sendTransaction',
+          params: [tx]
+        });
+        await waitTransaction({ txHash });
+      } catch (error) {
+        console.error('createListing error: ', error);
+        return;
+      }
     }
   };
 }
