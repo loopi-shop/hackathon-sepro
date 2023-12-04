9eb173f1def90349c03ebda5af2adf100d976233
alexrmsouza <alexmscontato@gmail.com>
2023-12-04 14:29:29 -0300

    feat: make visual modifications when click in frozen button

--- front-issuer/src/sections/tpf/tpf-holders.js
+++ front-issuer/src/sections/tpf/tpf-holders.js
@@ -1,4 +1,5 @@
-import { Dialog, DialogContent, DialogTitle, List, ListItem, ListItemText, ListItemButton } from "@mui/material"
+import { useState } from 'react';
+import { Dialog, DialogContent, DialogTitle, List, ListItem, ListItemButton, ListItemText } from "@mui/material"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLock, faLockOpen } from '@fortawesome/free-solid-svg-icons'
 import { useTPF } from "../../hooks/use-tpf";
@@ -6,6 +7,24 @@ import { enqueueSnackbar } from "notistack";
 
 export const TPFHolders = ({ open, handleClose, tpf, holders, setHolders }) => {
   if (!tpf?.symbol) return;
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Clientes - {tpf.symbol}</DialogTitle>
+      <DialogContent dividers={'paper'}>
+        <List
+          sx={{ width: '100%', maxWidth: 600, bgcolor: 'background.paper' }}
+          aria-label="clientes"
+        >
+          {holders.map(holder => TPFHolder(holder, setHolders, tpf))}
+        </List>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const TPFHolder = (holder, setHolders, tpf) => {
+  const [lockIcon, setLockIcon] = useState(faLockOpen);
 
   const { broadcast, setFrozen } = useTPF();
 
@@ -19,14 +38,14 @@ export const TPFHolders = ({ open, handleClose, tpf, holders, setHolders }) => {
       });
       console.info(`tx:setAddressFrozen:`, tx);
       const { txHash } = await broadcast({ tx });
-      enqueueSnackbar(`Transação de congelamento: ${txHash}`, {
-        variant: 'info',
-      });
+      enqueueSnackbar(`Transação: ${txHash}`, { variant: 'info' });
 
       setHolders((old) => {
         old.find(oldHolder => oldHolder.publicKey === holder.publicKey).isFrozen = !holder.isFrozen;
         return old;
       });
+
+      setLockIcon(holder.isFrozen ? faLockOpen : faLock);
     } catch (error) {
       console.error(`frozenHolder:`, error);
       enqueueSnackbar(`Erro ao congelar cliente ${holder.publicKey} para o token ${tpf.symbol}`, {
@@ -36,23 +55,11 @@ export const TPFHolders = ({ open, handleClose, tpf, holders, setHolders }) => {
   };
 
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Clientes - {tpf.symbol}</DialogTitle>
-      <DialogContent dividers={'paper'}>
-        <List
-          sx={{ width: '100%', maxWidth: 600, bgcolor: 'background.paper' }}
-          aria-label="clientes"
-        >
-          {holders.map((holder) => (
-            <ListItem key={holder.publicKey} disablePadding>
-              <ListItemText primary={holder.publicKey} />
-              <ListItemButton onClick={() => handleFrozen(holder)}>
-                <FontAwesomeIcon icon={holder.isFrozen ? faLock : faLockOpen} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </DialogContent>
-    </Dialog>
-  )
-}
+    <ListItem key={holder.publicKey} disablePadding>
+      <ListItemText primary={holder.publicKey}/>
+      <ListItemButton onClick={() => handleFrozen(holder)}>
+        <FontAwesomeIcon icon={lockIcon}/>
+      </ListItemButton>
+    </ListItem>
+  );
+};
